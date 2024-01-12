fbfe9873ecee9564d306352ffe9383f0042c4c99
Romano Ivan Pedro <romanoivanpedro@gmail.com>
2024-01-12 21:11:35 +0200

    Add project files.

---
+++ EnventosDB.MVC/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//++++++++++++++  CAROUSEL EDIT  +++++++++++++++++++++
+var homeCarousel = document.querySelector("#homeCarousel");
+var hCarousel = new bootstrap.Carousel(homeCarousel, {
+  interval: 2000,
+  pause: "hover",
+  touch: true,
+});
